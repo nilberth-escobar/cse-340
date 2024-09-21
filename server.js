d347220c5c51c9f852a518e5d6367b6e347a6956
nilberth-escobar <nilberrhescobar@mail.com>
2024-09-21 12:05:28 -0600

    server and templates setting up

--- server.js
+++ server.js
@@ -6,14 +6,27 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressEjsLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Template
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressEjsLayouts)
+app.set("layout", "./layouts/layout")
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+
+//Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
